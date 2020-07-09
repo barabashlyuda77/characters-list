5d0d0c6ece6967e72165af1807a8c1d1bedf9586
Lyuda Barabash <barabashlyuda@gmail.com>
2020-07-09 17:08:22 +0300

    Updates

---
+++ precache-manifest.8e3b6f4a068a8ecbe4ad7a98dfd8fc27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc521372d02db6c1749844b4677a8c9",
+    "url": "/characters-list/index.html"
+  },
+  {
+    "revision": "48fedde76822678da9f2",
+    "url": "/characters-list/static/css/main.01aa2dd1.chunk.css"
+  },
+  {
+    "revision": "3ba981cd9908ebf8d6e4",
+    "url": "/characters-list/static/js/2.6ec0a348.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/characters-list/static/js/2.6ec0a348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fedde76822678da9f2",
+    "url": "/characters-list/static/js/main.2f9ead2a.chunk.js"
+  },
+  {
+    "revision": "da37747ac864657e454e",
+    "url": "/characters-list/static/js/runtime-main.8a6eef0e.js"
+  }
+]);
